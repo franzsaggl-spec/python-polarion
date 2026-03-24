b4b01f045e41b886641a4e19b8f698324b3bc087
franz <franz@sag.ge>
2026-03-24 19:19:31 +0000

    refactor(v3): reset package to v3-only API surface

--- polarion/__init__.py
+++ polarion/__init__.py
@@ -1,50 +1,5 @@
-"""Polarion API client for Python."""
+"""python-polarion v3 package."""
 
-from .client import Polarion
-from .document import Document, DocumentCreator
-from .factory import register_creator
-from .plan import Plan, PlanCreator
-from .project import Project
-from .record import Record
-from .testrun import Testrun, TestrunCreator
-from .types import (
-    Approval,
-    AttachmentInfo,
-    CustomFieldValue,
-    EnumOption,
-    HyperlinkInfo,
-    LinkedItem,
-    PdfProperties,
-    TestStep,
-    TestStepResult,
-    TextContent,
-)
-from .user import User, UserCreator
-from .workitem import Workitem, WorkitemCreator
+from .v3 import PolarionClient
 
-register_creator("workitem", WorkitemCreator)
-register_creator("testrun", TestrunCreator)
-register_creator("user", UserCreator)
-register_creator("module", DocumentCreator)
-register_creator("plan", PlanCreator)
-
-__all__ = [
-    "Polarion",
-    "Project",
-    "Workitem",
-    "Document",
-    "Testrun",
-    "Record",
-    "Plan",
-    "User",
-    "TextContent",
-    "EnumOption",
-    "LinkedItem",
-    "Approval",
-    "CustomFieldValue",
-    "AttachmentInfo",
-    "HyperlinkInfo",
-    "TestStep",
-    "TestStepResult",
-    "PdfProperties",
-]
+__all__ = ["PolarionClient"]
--- polarion/soap/constants.py
+++
@@ -1,6 +0,0 @@
-"""Shared namespace constants for SOAP communication."""
-
-NS_SOAP = "http://schemas.xmlsoap.org/soap/envelope/"
-NS_XSI = "http://www.w3.org/2001/XMLSchema-instance"
-NS_XSD = "http://www.w3.org/2001/XMLSchema"
-NS_SESSION = "http://ws.polarion.com/session"
